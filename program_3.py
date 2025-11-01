c292ceeae2be8ece4a63dc64b1fa25a99cde69c7
Braden-Phetsarath <bkphetsarath@students.unwsp.edu>
2025-10-31 19:33:25 -0500

    Update program_3.py

--- program_3.py
+++ program_3.py
@@ -1,16 +1,31 @@
+#Braden Phetsarath
+#10/28
 # Program #3: Average Numbers
 # Assume a file containing a series of integers is named numbers.txt and exists on the computer's disk.
 # (please use the provided numbers.txt)
-# Write a program that reads all of the numbers stored in the file and calculates their total.  
+# Write a program that reads all of the numbers stored in the file and calculates their total.
 
-# The program should handle the following exceptions: 
+# The program should handle the following exceptions:
 
 # It should handle any IOError exceptions that are raised.
-# It should handle any ValueError exceptions that are raised when the items that are read from the file 
+# It should handle any ValueError exceptions that are raised when the items that are read from the file
 # are converted to a number.
 def sum_numbers_from_file():
     ######################
     # Add your code here #
+    with open("numbers.txt", "r") as file:
+        try:
+            total = 0
+            with open("numbers.txt", "r") as file:
+                for line in file:
+                    try:
+                        number = int(line.strip())
+                        total += number
+                    except ValueError:
+                        print("Invalid number found in file:{line.strip()}")
+            print("Total of all numbers in this file", total)
+        except IOError:
+            print("Error: Could not open/read file")
     ######################
     print('In the sum_numbers_from_file function')
 
